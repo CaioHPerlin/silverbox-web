3e1f68b878993d925baf05bb9661f5944cef94ef
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2026-09-10 21:42:30 -0400

    Criando arquivo Login

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Home from "./pages/Arquivos";
 import Arquivos from "./pages/Arquivos";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-       {/* home real: tela inicial estilo Google Drive */}
+        {/* home real: tela inicial estilo Google Drive */}
         <Route path="/" element={<Home />} />
- 
+        <Route path="/login" element={<Login />} />
+
         {/* listagem completa de arquivos */}
         <Route path="/arquivos" element={<Arquivos />} />
       </Routes>
